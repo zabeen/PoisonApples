ca8a4f3d8cf9f2d201c9c6621e49baeb9a7aa3db
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2017-12-13 19:42:00 +0000

    First attempt at solution for exercise 3C.07

---
+++ PoisonApples/ApplePoisoner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace PoisonApples
+{
+    public class ApplePoisoner
+    {
+        public IEnumerable<Apple> PickApples()
+        {
+            int colourIndex = 1;
+            int poisonIndex = 7;
+
+            while (true)
+            {
+                yield return new Apple
+                {
+                    Colour = GetColour(colourIndex),
+                    Poisoned = poisonIndex % 41 == 0
+                };
+
+                colourIndex += 5;
+                poisonIndex += 37;
+            }
+        }
+
+        private string GetColour(int colourIndex)
+        {
+            if (colourIndex % 13 == 0 || colourIndex % 29 == 0)
+            {
+                return "Green";
+            }
+
+            if (colourIndex % 11 == 0 || colourIndex % 19 == 0)
+            {
+                return "Yellow";
+            }
+
+            return "Red";
+        }
+
+        public class Apple
+        {
+            public string Colour { get; set; }
+            public bool Poisoned { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Colour} apple{(Poisoned ? " (poisoned!)" : "")}";
+            }
+        }
+    }
+}
---
+++ PoisonApples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoisonApples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var poisoner = new ApplePoisoner();
+            var apples = poisoner.PickApples().Take(10000).ToList();
+
+            Console.WriteLine($"No. of poisoned apples: {apples.Count(a => a.Poisoned)}");
+
+            Console.WriteLine($"Second most common colour of poisoned apples: {apples.Where(a => a.Poisoned).GroupBy(a => a.Colour).OrderByDescending(c => c.Count()).ElementAt(1).Key}");
+
+            Console.WriteLine($"Max. no. of non-poisoned Red apples picked in succession: {MaxCountSuccessiveRedNonPoisonedApples(ref apples)}");
+
+            Console.WriteLine($"Times two green apples picked in a row: {apples.Skip(1).Count(a => a.Colour == "Green" && apples[apples.IndexOf(a)-1].Colour == "Green")}");
+
+            Console.ReadLine();
+        }
+
+
+        static int MaxCountSuccessiveRedNonPoisonedApples(ref List<ApplePoisoner.Apple> apples)
+        {
+            int maxCount = 0;
+
+            for (int i = 0; i < apples.Count;)
+            {
+                int currentCount = 0;
+                if (!apples[i].Poisoned && apples[i].Colour == "Red")
+                    currentCount = apples.Skip(i).TakeWhile(a => !a.Poisoned && a.Colour == "Red").Count();
+
+                maxCount = (currentCount > maxCount) ? currentCount : maxCount;
+
+                i = i + currentCount + 1;
+            }
+
+            return maxCount;
+        }
+    }
+}
