43f6f454acf93c40c8a5f4788250c001fff0e344
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2017-12-13 20:19:22 +0000

    Max successive solution: replaced procedural function with LINQ

--- PoisonApples/Program.cs
+++ PoisonApples/Program.cs
@@ -24,31 +24,12 @@ namespace PoisonApples
                 .Key;
             Console.WriteLine($"Next most common colour of poisoned apples after Red: {nextColour}");
 
-            Console.WriteLine($"Max. no. of non-poisoned Red apples picked in succession: {MaxCountOfSuccessiveNonPoisonedRedApples(ref apples)}");
+            Func<ApplePoisoner.Apple, bool> filter = a => !a.Poisoned && a.Colour == "Red";
+            Console.WriteLine($"Max. no. of non-poisoned Red apples picked in succession: {apples.Where(filter).Select(a => apples.Skip(apples.IndexOf(a)).TakeWhile(filter).Count()).Max()}");
 
             Console.WriteLine($"Times two green apples picked in a row: {apples.Skip(1).Count(a => a.Colour == "Green" && apples[apples.IndexOf(a)-1].Colour == "Green")}");
 
             Console.ReadLine();
-        }
-
-
-        static int MaxCountOfSuccessiveNonPoisonedRedApples(ref List<ApplePoisoner.Apple> apples)
-        {
-            List<List<ApplePoisoner.Apple>> subLists = new List<List<ApplePoisoner.Apple>>();
-
-            for (int i = 0; i < apples.Count;)
-            {
-                List<ApplePoisoner.Apple> subList = new List<ApplePoisoner.Apple>();
-                if (!apples[i].Poisoned && apples[i].Colour == "Red")
-                {
-                    subList = apples.Skip(i).TakeWhile(a => !a.Poisoned && a.Colour == "Red").ToList();
-                    subLists.Add(subList);
-                }
-                
-                i = i + subList.Count + 1;
-            }
-
-            return subLists.Max(s => s.Count);
         }
     }
 }
